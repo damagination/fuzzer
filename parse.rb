379b643ec5194e513b40ecf36923b89c92cc77b9
Jarryd Lee <jarrydrlee@gmail.com>
2015-02-21 17:14:11 -0500

    Added parsing of arrays of urls

--- parse.rb
+++ parse.rb
@@ -4,7 +4,11 @@ load 'main.rb'
 
 def getFullPath(url)
   uri = URI(url)
-  uri.host + uri.path
+  returnUrl = uri.scheme + '://' + uri.host + uri.path
+  if not uri.port.to_s.empty?
+    port = ':' + uri.port.to_s
+  end
+  returnUrl + port
 end
 
 def getQueryParams(url)
@@ -19,6 +23,8 @@ end
 
 # Returns a hash of the query parameters for a given query string
 def queryToHash(query)
+  return {} if query.nil?
+
   queryPairs = Hash.new
   pairs = query.split('&')
   pairs.each do |pair|
@@ -28,3 +34,20 @@ def queryToHash(query)
   queryPairs
 end
 
+# Input: Array of urls (Strings)
+# Output: Hash of urls (key) with all the found query parameters
+def parseUrls(urls)
+  urlPairs = Hash.new( Hash.new )
+
+  urls.each do |url|
+    # Create new key value if key doesn't exist
+    # Else merge the query param hashs together
+    urlPairs[getFullPath(url)] = urlPairs[getFullPath(url)]
+                                    .merge(queryToHash(getQueryParams(url)))
+  end
+  urlPairs
+end
+
+# urlList = ['http://localhost:8081', 'http://localhost:8080/hello?2=2', 'http://localhost:8080/hello?1=1', 'http://localhost:8080?index=555']
+# p parseUrls(urlList)
+# returns => {"http://localhost:8081"=>{}, "http://localhost/hello:8080"=>{"2"=>"2", "1"=>"1"}, "http://localhost:8080"=>{"index"=>"555"}}
