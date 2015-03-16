c32823e73e141fb463149cd172b262b7a95cfd8d
Jared Smith <jared.rit@gmail.com>
2015-03-16 19:45:14 -0400

    Add input sanitization code, fixes #6

--- main.rb
+++ main.rb
@@ -76,7 +76,7 @@ def main
       end
       page = Page.new(@input_url, @auth_site)
       Page.add(page)
-      Page.crawl!
+      Page.crawl!(opts['vectors'])
       Page.guess(opts['common_words']) if opts['common_words']
     else
       puts @message
